0a859d176b8fa0ff3b3e6d8575701554d44b1ec7
rtanner-py <rob.tanner@gmail.com>
2024-12-18 08:33:49 +0000

    Create day_11.py

---
+++ day_11.py
@@ -0,0 +1,34 @@
+from collections import Counter
+from  datetime import datetime
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def calculate(item):
+    if item == 0:
+        return [1]
+    else:
+        digits = str(item)
+        mid, odd = divmod(len(digits), 2)
+        if odd:
+            return [item * 2024]
+        else:
+            return [int(digits[:mid]),int(digits[mid:])]
+
+def blink(counts, times):
+    for _ in range(times):
+        new = Counter()
+        for item, occurences in counts.items():
+            for newitem in calculate(item):
+                new[newitem] += occurences
+        counts = new
+    return counts
+
+start = datetime.now()
+# input = [125,17]
+# cycles = 25
+input = [64599,31,674832,2659361,1,0,8867,321]
+cycles = 75
+counts = Counter(input)
+counts = blink(counts,cycles)
+print(sum(counts.values()))
+print(f"Finished in {datetime.now() - start} seconds")
